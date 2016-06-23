b821b89218d48bd966f7301d076cdc101d5d94f8
Hans De Potter <hans_de_potter@hotmail.com>
2016-06-23 23:25:22 +0200

    Added globalDispatch and getGlobalState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-export default ({ dispatch, getState }) => (next) => (action) => {
+export default ({ dispatch, getState, globalDispatch, getGlobalState }) => (next) => (action) => {
 
     if (typeof action === 'function') {
-        return action(dispatch, getState)
+        return action(dispatch, getState, globalDispatch, getGlobalState)
     }
 
     return next(action)
