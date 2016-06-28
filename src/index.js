e1629ecf92602a737bd1151f9cb4151767cf712e
Hans De Potter <hans_de_potter@hotmail.com>
2016-06-28 19:02:25 +0200

    Updated to v 0.4.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-export default ({ dispatch, getState, globalDispatch, getGlobalState }) => (next) => (action) => {
+export default ({ localDispatch, getLocalState, globalDispatch, getGlobalState }) => (next) => (action) => {
 
     if (typeof action === 'function') {
-        return action(dispatch, getState, globalDispatch, getGlobalState)
+        return action(localDispatch, getLocalState, globalDispatch, getGlobalState)
     }
 
     return next(action)
